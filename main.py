73769c892c7e788981d42753b7c029bc5fb5d6c0
nazmihusin24590 <76603269+nazmihusin24590@users.noreply.github.com>
2024-06-22 10:45:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.express as px
 
 # Example GitHub raw data URL
-github_url = 'https://raw.githubusercontent.com/<nazmihusin24590>/water-dashboard/blob/main/Data.csv
+github_url = 'https://raw.githubusercontent.com/<nazmihusin24590>/water-dashboard/blob/main/Data.csv'
 
 # Load data from GitHub
 @st.cache  # Cache data for better performance
